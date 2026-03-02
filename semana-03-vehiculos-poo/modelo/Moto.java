538880218217b476f6428e8fa2904dab5a746fb7
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-01 20:14:54 -0400

    feat: implementar Auto, Moto, Camion con @Override

---
+++ semana-03-vehiculos-poo/modelo/Moto.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Moto extends Vehiculo {
+    private boolean tieneSidecar;
+
+    public Moto(String marca, String modelo, int anio, boolean sidecar) {
+        super(marca, modelo, anio);
+        this.tieneSidecar = sidecar;
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println(marca + " (moto) acelera ligero.");
+    }
+
+    public boolean isTieneSidecar() {
+        return tieneSidecar;
+    }
+}
