3d757712196da4b995130ec17c5684b8c5897df4
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-02 00:10:05 -0400

    style: eliminar espaciado excesivo

--- semana-03-vehiculos-poo/Main.java
+++ semana-03-vehiculos-poo/Main.java
@@ -6,32 +6,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        // ==========================================
-        // === DEMOSTRACION DE POLIMORFISMO (RF6) ===
-        // ==========================================
-
         ArrayList<Vehiculo> flota = new ArrayList<>();
-
         flota.add(new Auto("Toyota", "Corolla", 2024, 4));
         flota.add(new Moto("Honda", "CB500", 2023, false));
         flota.add(new Camion("Volvo", "FH16", 2022, 24.0));
         flota.add(new AutoElectrico("Tesla", "Model 3", 2024, 4, 80));
         flota.add(new MotoElectrica("NIU", "MQi+", 2023, 60));
-
         System.out.println("=== acelerar() en toda la flota ===");
-
         for (Vehiculo v : flota) {
-            v.acelerar();   // Cada objeto ejecuta su propia version
+            v.acelerar();   
         }
 
-        // ==========================================
-        // ============ MENU INTERACTIVO ============
-        // ==========================================
-
         GestorVehiculos gestor = new GestorVehiculos();
-
-        // Cargamos algunos vehiculos iniciales al gestor
         gestor.agregar(new Auto("Toyota", "Corolla", 2024, 4));
         gestor.agregar(new Moto("Honda", "CB500", 2023, false));
         gestor.agregar(new Camion("Volvo", "FH16", 2022, 24.0));
@@ -52,68 +38,88 @@ public class Main {
             System.out.println("7. Estadisticas");
             System.out.println("8. Salir");
             System.out.print("Opcion: ");
-
             opcion = sc.nextInt();
-
             switch (opcion) {
-
                 case 1:
-                    System.out.println("Tipo (auto/moto/camion/autoE/motoE): ");
-                    String tipo = sc.next();
+                    System.out.println("\nSeleccione tipo de vehiculo:");
+                    System.out.println("1. Auto");
+                    System.out.println("2. Moto");
+                    System.out.println("3. Camion");
+                    System.out.println("4. Auto Electrico");
+                    System.out.println("5. Moto Electrica");
+                    System.out.print("Opcion: ");
 
-                    if (tipo.equalsIgnoreCase("auto")) {
-                        gestor.agregar(new Auto("Ford", "Focus", 2020, 4));
-                    }
-                    else if (tipo.equalsIgnoreCase("moto")) {
-                        gestor.agregar(new Moto("Yamaha", "R3", 2022, false));
-                    }
-                    else if (tipo.equalsIgnoreCase("camion")) {
-                        gestor.agregar(new Camion("Scania", "R500", 2021, 18.0));
-                    }
-                    else if (tipo.equalsIgnoreCase("autoE")) {
-                        gestor.agregar(new AutoElectrico("BYD", "Dolphin", 2024, 4, 50));
-                    }
-                    else if (tipo.equalsIgnoreCase("motoE")) {
-                        gestor.agregar(new MotoElectrica("SuperSoco", "TC", 2023, 40));
+                    int tipo = sc.nextInt();
+
+                    switch (tipo) {
+                        case 1:
+                            gestor.agregar(new Auto("Ford", "Focus", 2020, 4));
+                            break;
+                        case 2:
+                            gestor.agregar(new Moto("Yamaha", "R3", 2022, false));
+                            break;
+                        case 3:
+                            gestor.agregar(new Camion("Scania", "R500", 2021, 18.0));
+                            break;
+                        case 4:
+                            gestor.agregar(new AutoElectrico("BYD", "Dolphin", 2024, 4, 50));
+                            break;
+                        case 5:
+                            gestor.agregar(new MotoElectrica("SuperSoco", "TC", 2023, 40));
+                            break;
+                        default:
+                            System.out.println("Opcion invalida.");
                     }
                     break;
-
                 case 2:
                     gestor.listarTodos();
                     break;
-
                 case 3:
-                    System.out.print("Ingrese tipo (auto/moto/camion/electrico): ");
-                    String filtro = sc.next();
-                    gestor.listarPorTipo(filtro);
-                    break;
+                    System.out.println("\nFiltrar por:");
+                    System.out.println("1. Auto");
+                    System.out.println("2. Moto");
+                    System.out.println("3. Camion");
+                    System.out.println("4. Electrico");
+                    System.out.print("Opcion: ");
 
+                    int filtro = sc.nextInt();
+
+                    switch (filtro) {
+                        case 1:
+                            gestor.listarPorTipo("auto");
+                            break;
+                        case 2:
+                            gestor.listarPorTipo("moto");
+                            break;
+                        case 3:
+                            gestor.listarPorTipo("camion");
+                            break;
+                        case 4:
+                            gestor.listarPorTipo("electrico");
+                            break;
+                        default:
+                            System.out.println("Opcion invalida.");
+                    }
+                    break;
                 case 4:
                     gestor.listarElectricos();
                     break;
-
                 case 5:
                     gestor.cargarElectricosNecesarios();
                     break;
-
                 case 6:
                     gestor.demostrarPolimorfismo();
                     break;
-
                 case 7:
                     gestor.mostrarEstadisticas();
                     break;
-
                 case 8:
                     System.out.println("Saliendo del sistema...");
                     break;
-
                 default:
                     System.out.println("Opcion invalida.");
             }
-
         } while (opcion != 8);
-
         sc.close();
     }
 }
