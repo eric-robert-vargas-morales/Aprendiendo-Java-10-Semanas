1a2a55b9fff7185e4810731ccc0cbd0fa0423706
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-08 15:59:43 -0400

    feat: operaciones agregar, buscar y eliminar

--- semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
+++ semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
@@ -16,5 +16,39 @@ public class GestorEstudiantes {
         historial=new ArrayList<>();
 
     }
+        public boolean agregar(Estudiante e){
+        if(porCarnet.containsKey(e.getCarnet())){
+            return false;
+        }
+        porCarnet.put(e.getCarnet(), e);
+        ranking.add(e);
+        historial.add("AGREGAR : " +e.getCarnet() + " - " + e.getNombre());
+        return true;
+    }
+
+    public Estudiante buscar(String carnet){
+        return porCarnet.get(carnet);
+    }
+
+    public boolean actualizarPromedio(String carnet, double nuevoProm){
+        Estudiante e= porCarnet.get(carnet);
+        if(e==null)
+            return false;
+        ranking.remove(e);
+        e.setPromedio(nuevoProm);
+        ranking.add(e);
+
+        historial.add("ACTUALIZAR: " + carnet + " promedio -> " + nuevoProm);
+        return true;
+    }
+    
+    public boolean eliminar(String carnet){
+        Estudiante e=porCarnet.remove(carnet);
+        if(e==null)
+            return false;
+        ranking.remove(e);
+        historial.add("ELIMINAR: " + carnet + " - " + e.getNombre());
+        return true;
+    }
 
 }
