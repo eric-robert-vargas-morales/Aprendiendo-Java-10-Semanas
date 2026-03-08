2dc20118131f352a9d60096fee6939863eed37fd
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-08 15:57:50 -0400

    feat: Gestor de Estudiantes con HashMap, TreeSet y ArrayList

---
+++ semana-04-estudiantes-collections/servicio/GestorEstudiantes.java
@@ -0,0 +1,20 @@
+package servicio;
+
+import modelo.Estudiante;
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.ArrayList;
+public class GestorEstudiantes {
+
+    private HashMap<String, Estudiante> porCarnet;
+    private TreeSet<Estudiante> ranking;
+    private ArrayList<String> historial;
+
+    public GestorEstudiantes(){
+        porCarnet=new HashMap<>();
+        ranking=new TreeSet<>();
+        historial=new ArrayList<>();
+
+    }
+
+}
