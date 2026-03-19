a91618c3bf8da4f7f56b05f67318957a48de6e64
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-19 16:01:23 -0400

    feat: excepciones ContactoExistente, NoEncontrado y DatoInvalido

---
+++ semana-05-agenda-contactos/agenda-contactos/src/main/java/exception/ContactoExistenteException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class ContactoExistenteException extends Exception{
+    private String idDuplicado;
+
+    public ContactoExistenteException(String id) {
+        super("Ya existe un contacto con el ID: " + id);
+        this.idDuplicado = id;
+    }
+
+    public String getIdDuplicado() {
+        return idDuplicado;
+    }
+}
