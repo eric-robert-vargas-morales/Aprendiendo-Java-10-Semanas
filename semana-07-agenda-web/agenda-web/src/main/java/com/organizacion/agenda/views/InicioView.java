94895580d39b4053e125431a64ee5942f8d48ba7
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-29 17:42:44 -0400

    feat: crear InicioView con titulo y descripcion

--- semana-07-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
+++ semana-07-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
@@ -1,6 +1,9 @@
 package com.organizacion.agenda.views;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -11,7 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InicioView extends VerticalLayout{
 
     public InicioView(){
-        H1 titulo = new H1("Hola eric");
-        add(titulo);
+        setPadding(true);
+        setSpacing(true);
+
+        H1 titulo = new H1("Agenda de Contactos");
+        H2 subtitulo = new H2("Tu agenda personal ");
+        Paragraph descripcion =  new Paragraph("Guarda, edita y busca los contactos en un solo lugar");
+
+        Button verContactos = new Button("Ver contactos");
+        verContactos.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("contactos")));
+
+
+        add(titulo, subtitulo, descripcion, verContactos);
     }
 }
