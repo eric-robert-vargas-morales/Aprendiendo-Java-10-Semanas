5ba5278ad3c9ea19cc0f9f8251193788d0b138db
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-29 12:57:33 -0400

    feat: agregar Application.java con SpringBootApplication

--- semana-07-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
+++ semana-07-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
@@ -1,5 +1,17 @@
 package com.organizacion.agenda.views;
 
-public class InicioView {
-    
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Route("")
+public class InicioView extends VerticalLayout{
+
+    public InicioView(){
+        H1 titulo = new H1("Hola eric");
+        add(titulo);
+    }
 }
