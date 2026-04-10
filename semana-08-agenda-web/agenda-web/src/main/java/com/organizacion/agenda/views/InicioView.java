0f208a41b30d34feb82b6d5249c02bf55d9a46a2
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-04-10 16:19:05 -0400

    refactor: InicioView con H2, Paragraph y H3

--- semana-08-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
+++ semana-08-agenda-web/agenda-web/src/main/java/com/organizacion/agenda/views/InicioView.java
@@ -1,30 +1,38 @@
 package com.organizacion.agenda.views;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
+import com.organizacion.agenda.ui.MainLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@Route("")
+@Route(value = "", layout = MainLayout.class)
 public class InicioView extends VerticalLayout{
 
     public InicioView(){
+        setSizeFull();
         setPadding(true);
-        setSpacing(true);
 
-        H1 titulo = new H1("Agenda de Contactos");
-        H2 subtitulo = new H2("Tu agenda personal ");
-        Paragraph descripcion =  new Paragraph("Guarda, edita y busca los contactos en un solo lugar");
+        VerticalLayout contenido = new VerticalLayout();
+        contenido.setPadding(false);
+        contenido.setAlignItems(FlexComponent.Alignment.START);
 
-        Button verContactos = new Button("Ver contactos");
-        verContactos.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("contactos")));
+        H2 titulo = new H2("Bienvenido a tu Agenda");
+        
+        Paragraph descripcion =  new Paragraph("Guarda y organiza tus contactos en un solo lugar " + "Busca por nombre, agrega nuevos contactos y " + "manten tu lista siempre actualizada");
 
+        H3 instruccion = new H3("Usa el menu superior para navegar");
 
-        add(titulo, subtitulo, descripcion, verContactos);
+        contenido.add(titulo, descripcion, instruccion);
+
+        Div footer = new Div(new Span("Agenda de contactos v1.0"));
+        footer.setWidthFull();
+
+        add(contenido, footer);
+        expand(contenido);
     }
 }
